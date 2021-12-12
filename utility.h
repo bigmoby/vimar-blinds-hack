939eeb09250473c08f7d749f0afc4403675e8681
Fabio Bigmoby Mauro <ing.fabiomauro@gmail.com>
2021-12-12 18:50:00 +0100

    fix relay stop

--- utility.h
+++ utility.h
@@ -7,28 +7,30 @@ void reset_selettore_tapparelle() {
 }
 
 void set_selettore_tapparelle(int scena_tapparella_attuale, int scena_tapparella_voluta) {
-  delay(11000);
+  delay(13000);
 
   id(relayStop).turn_on();
-  delay(400);
+  delay(200);
   id(relayStop).turn_off();
 
-  delay(1000);
+  delay(500);
 
   int step = scena_tapparella_voluta - scena_tapparella_attuale;
 
   if (step > 0) {
     for (int i = 0; i < step; i++) {
       id(relaySelectPlus).turn_on();
-      delay(400);
+      delay(150);
       id(relaySelectPlus).turn_off();
+      delay(150);
     }
   } else {
     int abs_value_step = abs(step);
     for (int i = 0; i < abs_value_step; i++) {
       id(relaySelectMinus).turn_on();
-      delay(400);
+      delay(150);
       id(relaySelectMinus).turn_off();
+      delay(150);
     }
   }
 }
