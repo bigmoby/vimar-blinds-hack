eac793ab00d7956b149b206048bf76208b1bd19e
Fabio Bigmoby Mauro <ing.fabiomauro@gmail.com>
2022-04-20 21:49:38 +0200

    reset for each tapparella select

--- utility.h
+++ utility.h
@@ -6,8 +6,17 @@ void reset_selettore_tapparelle() {
   }
 }
 
-void set_selettore_tapparelle(int scena_tapparella_attuale, int scena_tapparella_voluta) {
-  delay(13000);
+void set_selettore_tapparelle(int scena_tapparella_voluta) {
+  
+  id(relay8).turn_on(); // Disalimento il telecomando
+  delay(200);
+  id(relayStop).turn_on(); // workaround per fare scaricare il circuito dalla corrente residua
+  delay(500);
+  id(relayStop).turn_off();
+  delay(200);
+  id(relay8).turn_off();
+  
+  delay(200);
 
   id(relayStop).turn_on();
   delay(200);
@@ -15,22 +24,14 @@ void set_selettore_tapparelle(int scena_tapparella_attuale, int scena_tapparella
 
   delay(500);
 
+  int scena_tapparella_attuale = 1; // dopo il reset il telecomando parte sempre da 1
   int step = scena_tapparella_voluta - scena_tapparella_attuale;
-
-  if (step > 0) {
-    for (int i = 0; i < step; i++) {
-      id(relaySelectPlus).turn_on();
-      delay(150);
-      id(relaySelectPlus).turn_off();
-      delay(150);
-    }
-  } else {
-    int abs_value_step = abs(step);
-    for (int i = 0; i < abs_value_step; i++) {
-      id(relaySelectMinus).turn_on();
-      delay(150);
-      id(relaySelectMinus).turn_off();
-      delay(150);
-    }
+  
+  for (int i = 0; i < step; i++) {
+    id(relaySelectPlus).turn_on();
+    delay(150);
+    id(relaySelectPlus).turn_off();
+    delay(150);
   }
+ 
 }
